8a0b39a344db524140d58f5cf43a56cc8a36ae8f
Carlos André <31482507+carlos-dev@users.noreply.github.com>
2021-07-15 01:56:31 -0300

    Config: configuring reactotron and eslint

---
+++ src/pages/main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const main = () => (
+  <View>
+    <View />
+  </View>
+);
+
+const styles = StyleSheet.create({});
+
+export default main;

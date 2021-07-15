72d5e1ce8f67118d98f2b9b14fddd5f9f443f0e5
Carlos André <31482507+carlos-dev@users.noreply.github.com>
2021-07-15 02:17:58 -0300

    Config: configuring react-navigation

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 
 const main = () => (
   <View>
-    <View />
+    <Text>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis, fugiat hic tempore veniam sed cumque odio perferendis alias aliquid sequi ullam, a cum dolorum mollitia repudiandae obcaecati, atque quasi asperiores!</Text>
   </View>
 );
 

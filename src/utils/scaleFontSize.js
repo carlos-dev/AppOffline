c047fedb86b96f5cc072785c79c5330c55a53c93
Carlos André <31482507+carlos-dev@users.noreply.github.com>
2021-07-15 09:22:00 -0300

    Feature: function that makes the font responsive

---
+++ src/utils/scaleFontSize.js
@@ -0,0 +1,12 @@
+import { Dimensions } from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width; // get current width
+const SCALE = 325; // constant, 325 is standard width of  iphone 5
+
+const scaleFontSize = (fontSize) => {
+  const ratio = fontSize / SCALE; // get ratio based on your standard scale
+  const newSize = Math.round(ratio * SCREEN_WIDTH);
+  return newSize;
+};
+
+export default scaleFontSize;
